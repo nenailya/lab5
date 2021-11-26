8257d49ad9e044f10c1cf22fe83841687f792754
n_zgr <nzgr1@mail.ru>
2021-11-27 02:58:08 +0300

    executor

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,20 +1,15 @@
 package com.example.lab5
 
-import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lab5.databinding.ActivityMainBinding
-import java.net.URL
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-import android.graphics.BitmapFactory
 import android.util.Log
-import androidx.lifecycle.MutableLiveData
+import android.widget.Button
+import android.widget.ImageView
+
 
 class MainActivity : AppCompatActivity() {
-    private val executor: ExecutorService = Executors.newSingleThreadExecutor()
-    private val img = "https://thiscatdoesnotexist.com/"
-    private var bitmap: MutableLiveData<Bitmap> = MutableLiveData()
+    private val viewModel = ViewModel()
 
     private lateinit var binding: ActivityMainBinding
 
@@ -22,44 +17,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        if (savedInstanceState != null) bitmap.value =
-            savedInstanceState.getParcelable("bitmap")
-
-        binding.download.setOnClickListener {
-            download(URL(img))
+        val imageView: ImageView = findViewById(R.id.imageView)
+        val download: Button = findViewById(R.id.download)
+        download.setOnClickListener {
+            viewModel.download()
         }
-        bitmap.observe(this) {
-            binding.imageView.setImageBitmap(it)
+        viewModel.bitmap.observe(this) {
+            imageView.setImageBitmap(it)
         }
-        log("onCreate")
-    }
-
-    private fun download(url: URL) {
-        executor.execute {
-            log("sleeping")
-            Thread.sleep(1000)
-            log("downloading in ${Thread.currentThread()}")
-
-            bitmap.postValue(BitmapFactory.decodeStream(url.openConnection().getInputStream()))
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putParcelable("bitmap", bitmap.value)
+        Log.d("onCreate", "onCreate")
     }
 
     override fun onStop() {
-        executor.shutdown()
+        Thread.currentThread().interrupt()
         super.onStop()
-        log("onStop")
-    }
-
-    private val tag = "myTag"
-
-    private fun log(msg: String) {
-        Log.d(tag, msg)
     }
 
 }
